bfe0d7d0889971dddc7d25ab51983fcd861855ab
Cam Hutchison <camh@goteleport.com>
2024-07-09 17:22:21 +1000

    build: Find feature headers outside system include dir
    Look for PAM and BPF header files that control including features in
    teleport binaries more generally instead of assuming they are in the
    system include headers or the one location in the buildbox (for libbpf
    in `/usr/libbpf-1.2.2`).
    
    This allows a cross-compiler to be used that sets the standard
    environment variables `C_INCLUDE_PATH` and `LIBRARY_PATH` to point to
    where additional headers and libraries live.
    
    This cleans up the BPF detection in `common.mk` as it had some
    head-scratching failure modes when clang or llvm-strip were not found.
    The logic for enabling BPF is clearer now and safer.

--- common.mk
+++ common.mk
@@ -1,33 +1,58 @@
 # Common makefile shared between Teleport OSS and Ent.
 
+# -----------------------------------------------------------------------------
+# libbpf detection
+#
+# Requirements for building with BFP support:
+# * clang and llvm-strip programs
+# * linux on amd64 or amd64
+# * libbpf (the bpf/bpf.h header file)
+#   * The centos7 buildbox puts this in /usr/libbpf-1.2.2
+#   * The ng buildbox puts this on C_INCLUDE_PATH
+#   * Native/local builds has this at /usr/include
+# * Either using a cross-compiling buildbox or is a native build
+#
+# The default is without BPF support unless all the critera are met.
+#
+# TODO(camh): Remove /usr/libbpf-1.2.2 when old buildboxes are replaced by ng
+
+with_bpf := no
+BPF_MESSAGE := without-BPF-support
+
+CLANG ?= $(shell which clang || which clang-12)
+LLVM_STRIP ?= $(shell which llvm-strip || which llvm-strip-12)
+
 # libbpf version required by the build.
 LIBBPF_VER := 1.2.2
+
+LIBBPF_HEADER_CANDIDATES := $(subst :, ,$(C_INCLUDE_PATH)) /usr/libbpf-$(LIBBPF_VER)/include /usr/include
+LIBBPF_H := $(firstword $(wildcard $(addsuffix /bpf/bpf.h,$(LIBBPF_HEADER_CANDIDATES))))
 
 # Is this build targeting the same OS & architecture it is being compiled on, or
 # will it require cross-compilation? We need to know this (especially for ARM) so we
 # can set the cross-compiler path (and possibly feature flags) correctly.
-IS_NATIVE_BUILD ?= $(if $(filter $(ARCH), $(shell go env GOARCH)),"yes","no")
+IS_NATIVE_BUILD ?= $(filter $(ARCH),$(shell go env GOARCH))
+IS_CROSS_COMPILE_BB = $(filter $(BUILDBOX_MODE),cross)
 
-# BPF support will only be built into Teleport if headers exist at build time.
-BPF_MESSAGE := without-BPF-support
+# Only build with BPF if clang and llvm-strip are installed.
+ifneq (,$(and $(CLANG),$(LLVM_STRIP)))
 
-# We don't compile BPF for anything except linux/amd64 and linux/arm64 for now,
-# as other builds have compilation issues that require fixing.
-with_bpf := no
-ifeq ("$(OS)","linux")
-# True if $ARCH == amd64 || $ARCH == arm64
-ifneq (,$(filter "$(ARCH)","amd64" "arm64"))
-# We only support BPF in native builds
-ifeq ($(IS_NATIVE_BUILD),"yes")
-ifneq ("$(wildcard /usr/libbpf-${LIBBPF_VER}/include/bpf/bpf.h)","")
+# Only build with BPF for linux/amd64 and linux/arm64.
+# Other builds have compilation issues that require fixing.
+ifneq (,$(filter $(OS)/$(ARCH),linux/amd64 linux/arm64))
+
+# Only build with BPF if we found the bpf.h header file
+ifneq (,$(LIBBPF_H))
+
+# Only build with BPF if its a native build or in a cross-compiling buildbox.
+ifneq (,$(or $(IS_NATIVE_BUILD),$(IS_CROSS_COMPILE_BB)))
+
 with_bpf := yes
 BPF_TAG := bpf
 BPF_MESSAGE := with-BPF-support
-CLANG ?= $(shell which clang || which clang-12)
-LLVM_STRIP ?= $(shell which llvm-strip || which llvm-strip-12)
 KERNEL_ARCH := $(shell uname -m | sed 's/x86_64/x86/g; s/aarch64/arm64/g')
-INCLUDES :=
 ER_BPF_BUILDDIR := lib/bpf/bytecode
+BPF_INCLUDES := -I/usr/libbpf-$(LIBBPF_VER)/include
 
 # Get Clang's default includes on this system. We'll explicitly add these dirs
 # to the includes list when compiling with `-target bpf` because otherwise some
@@ -40,11 +65,34 @@ ER_BPF_BUILDDIR := lib/bpf/bytecode
 CLANG_BPF_SYS_INCLUDES = $(shell $(CLANG) -v -E - </dev/null 2>&1 \
 	| sed -n '/<...> search starts here:/,/End of search list./{ s| \(/.*\)|-idirafter \1|p }')
 
-STATIC_LIBS += -L/usr/libbpf-${LIBBPF_VER}/lib64/ -lbpf -lelf -lz
-# Link static version of libraries required by Teleport (bpf, pcsc) to reduce system dependencies. Avoid dependencies on dynamic libraries if we already link the static version using --as-needed.
-CGOFLAG = CGO_ENABLED=1 CGO_CFLAGS="-I/usr/libbpf-${LIBBPF_VER}/include" CGO_LDFLAGS="-Wl,-Bstatic $(STATIC_LIBS) -Wl,-Bdynamic -Wl,--as-needed"
+# Add library path for CentOS 7 buildbox.
+# TODO(camh): Remove when that buildbox is decomissioned.
+STATIC_LIBS += -L/usr/libbpf-${LIBBPF_VER}/lib64/
+
+# gcc does not use LIBRARY_PATH when cross-compiling, so explicitly add those
+# paths to the linker command line.
+STATIC_LIBS += $(addprefix -L,$(subst :, ,$(LIBRARY_PATH)))
+
+# Libraries needed for or by BPF
+STATIC_LIBS += -lbpf -lelf -lz
+
+# Link static version of libraries required by Teleport (bpf, pcsc) to reduce
+# system dependencies. Avoid dependencies on dynamic libraries if we already
+# link the static version using --as-needed.
+CGOFLAG = CGO_ENABLED=1 CGO_CFLAGS="$(BPF_INCLUDES)" CGO_LDFLAGS="-Wl,-Bstatic $(STATIC_LIBS) -Wl,-Bdynamic -Wl,--as-needed"
 CGOFLAG_TSH = CGO_ENABLED=1 CGO_LDFLAGS="-Wl,-Bstatic $(STATIC_LIBS_TSH) -Wl,-Bdynamic -Wl,--as-needed"
+
+endif # IS_NATIVE_BUILD || IS_CROSS_COMPILE_BB
 endif # bpf/bpf.h found
-endif # IS_NATIVE_BUILD == yes
-endif # ARCH == amd64 OR arm64
-endif # OS == linux
+endif # OS/ARCH == linux/amd64 OR linux/arm64
+endif # clang and llvm-strip found
+
+.PHONY: diag-bpf-vars
+diag-bpf-vars:
+	@echo clang: $(CLANG)
+	@echo llvm-strip: $(LLVM_STRIP)
+	@echo os/arch: $(OS)-$(ARCH)
+	@echo bpf.h: $(LIBBPF_H)
+	@echo is-native: $(IS_NATIVE_BUILD)
+	@echo is-cross: $(IS_CROSS_COMPILE_BB)
+	@echo buildbox-mode: $(BUILDBOX_MODE)
